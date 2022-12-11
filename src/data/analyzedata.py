1b9a21426b1f6f3620ee87320ee550b34a3cb3eb
bluepower9 <jarrod.leong@yahoo.com>
2022-12-10 18:28:16 -0800

    updated README

--- src/data/analyzedata.py
+++ src/data/analyzedata.py
@@ -59,15 +59,3 @@ def aggregate_data(state: list, db_name:str = "chess", collection:str = 'games',
     return db[db_name][collection].aggregate(pipeline)
 
 
-
-#for testing
-if __name__ == '__main__':
-    board = chess.Board()
-    fens = {}
-    for b in board.legal_moves:
-        board.push(b)
-        fens[board.fen()] = b
-        board.pop()
-
-    data = list(aggregate_data(list(fens.keys()), collection='games3', format=['bullet'], min_elo=600, max_elo=800))
-    print([fens[fen['_id']['game_state']] for fen in data])
